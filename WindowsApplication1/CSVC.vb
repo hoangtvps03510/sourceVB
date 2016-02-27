fabf71fda01c73ea4a27e3530caaefe472d977a3
hoangtvps03510 <hoangtvps03510@fpt.edu.vn>
2016-02-27 16:47:43 +0700

    bvnb

---
+++ WindowsApplication1/CSVC.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmCSVC
+
+End Class
---
+++ WindowsApplication1/main.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmmain
+
+    Private Sub tslbl1_Click(sender As Object, e As EventArgs) Handles tslbl1.Click
+        frmsanpham.Show()
+    End Sub
+
+    Private Sub tslblkh_Click(sender As Object, e As EventArgs) Handles tslblkh.Click
+        frmkhachhang.Show()
+    End Sub
+    Private Sub Tslblgioithieu_Click(sender As Object, e As EventArgs) Handles tslblgioithieu.Click
+        frmCSVC.Show()
+    End Sub
+End Class
